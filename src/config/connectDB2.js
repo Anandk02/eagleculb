5d4b91760c0cde6be304470dc4af51fe6441ac87
anand harma <you@example.com>
2024-03-06 03:38:08 +0530

    first commit

---
+++ src/config/connectDB2.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    port: '3307',
+    password:'',
+    database: 'contact'
+});
+
+// const connection = mysql.createPool({
+//     host: 'localhost',
+//     user: 'admin',
+//     password: 'Dph51mO5qkS8U1k',
+//     database: '92lottery'
+// });
+
+connection.getConnection((err, conn) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL');
+    conn.release(); // Release the connection when done
+});
+
+console.log('connect'+connection);
+export default connection;
